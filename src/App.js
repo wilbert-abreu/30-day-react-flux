d582eb4000833d185575da8454989abba63b5882
Wilbert Abreu <wilbert.abreu@complex.com>
2016-12-26 18:06:18 -0500

    Nesting Routes

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import './App.css';
 
-import { Router, Route, hashHistory } from 'react-router'
-
+import {
+  Router, Route, hashHistory, IndexRoute
+} from 'react-router';
 import { Index } from './containers/Index';
 
-const Home = () => (  <div><h1>Welcome home</h1></div> )
-const About = () => (<div><h1>About</h1></div> )
+const Home = () => ( <div><h1>Welcome home</h1></div> )
+const About = () => ( <div><h1>About</h1></div> )
 
 export class App extends React.Component {
   render() {
     return (
       <Router history={ hashHistory }>
         <Route path="/" component={ Index }>
+          <IndexRoute component={ Home } />
           <Route path="home" component={ Home } />
           <Route path="about" component={ About } />
         </Route>
