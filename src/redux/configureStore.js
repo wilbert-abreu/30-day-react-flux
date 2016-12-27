9c8af07d03548655a227c4206ec2ba57f4438678
Wilbert Abreu <wilbert.abreu@complex.com>
2016-12-26 21:05:31 -0500

    created reducers

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,7 +1,10 @@
 import {createStore} from 'redux';
+// import all the exports in `currentTime`
+// to an object called `currentTime`
+import * as currentTime from './modules/currentTime';
 
 export const configureStore = () => {
-  const store = createStore();
+  const store = createStore(currentTime.reducer);
 
   return store;
 }
---
+++ src/redux/modules/currentTime.js
@@ -0,0 +1,7 @@
+const initialState = {
+  currentTime: new Date()
+}
+
+export const reducer = (state = initialState, action) => {
+  return state;
+}
